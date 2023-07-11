dcb66f7192b25d5fec7aa3ac9df7c2c118f90fd4
Adrian Richard <109044908+adrianrichard@users.noreply.github.com>
2023-07-10 21:39:27 -0300

    nada

--- GUILogin/util/generic.py
+++ GUILogin/util/generic.py
@@ -1,12 +1,12 @@
 from PIL import ImageTk, Image
 
 def leer_imagen( path, size): 
-        return ImageTk.PhotoImage(Image.open(path).resize(size, Image.ANTIALIAS))  
+        return ImageTk.PhotoImage(Image.open(path).resize(size, Image.Resampling.LANCZOS))  
 
 
 def centrar_ventana(ventana,aplicacion_ancho,aplicacion_largo):    
-    pantall_ancho = ventana.winfo_screenwidth()
-    pantall_largo = ventana.winfo_screenheight()
-    x = int((pantall_ancho/2) - (aplicacion_ancho/2))
-    y = int((pantall_largo/2) - (aplicacion_largo/2))
+    pantalla_ancho = ventana.winfo_screenwidth()
+    pantalla_largo = ventana.winfo_screenheight()
+    x = int((pantalla_ancho/2) - (aplicacion_ancho/2))
+    y = int((pantalla_largo/2) - (aplicacion_largo/2))
     return ventana.geometry(f"{aplicacion_ancho}x{aplicacion_largo}+{x}+{y}")
